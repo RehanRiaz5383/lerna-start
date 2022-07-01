b84342061e33182a879e912a3d83e232da1df3ab
Rehan Riaz <rehan5383@gmail.com>
2022-07-01 12:44:22 +0500

    lerna-r-and-d

---
+++ common/dist/index.tsx
@@ -0,0 +1,3 @@
+import Main from './src/App';
+
+export {Main}
---
+++ products/elearn/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+ import {Main} from 'dist';
+
+function App() {
+  return (
+    <>
+     <Main/> 
+    </>
+  );
+}
+
+export default App;
