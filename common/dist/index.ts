22ee44ab84b59af68b87ddc5a98a27a528244763
Rehan Riaz <rehan5383@gmail.com>
2022-07-13 17:32:56 +0500

    full skeleton ready

---
+++ common/dist/index.ts
@@ -0,0 +1,12 @@
+import MyComponent from "./MyComponent";
+
+const info = {
+  title: "Hello world",
+  hello: "Hello",
+  world: "World",
+  welcome: "Welcome",
+  description: "description",
+};
+
+export { MyComponent };
+export { info };
--- products/elearn/src/App.tsx
+++ products/elearn/src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
- import {App} from 'dist';
+import {MyComponent,info} from 'dist';
+import i18next  from 'i18next';
+import { Suspense } from 'react';
+import { initReactI18next, useTranslation } from 'react-i18next';
+import {en,es} from 'lang';
+
+
+i18next.use(initReactI18next)
+.init({
+  resources:{
+    en:{translation:en},
+    es:{translation:es}
+    //sp:transactionSp
+  },
+  lng:"es",
+  fallbackLng:"en",
+  interpolation:{escapeValue:false}
+})
 
 function MyApp() {
+  const {t} = useTranslation()
   return (
-    <>
-     <App/>
-    </>
+    <Suspense fallback="Loading....">
+     <MyComponent title={t('welcome')}/>
+     <p>{t('description')}</p>
+    </Suspense>
   );
 }
 
